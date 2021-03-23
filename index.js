7c3c4b779e2d7903be0cc481327f4569d5399e77
Kyoko <kyokoconnect@gmail.com>
2021-03-23 19:01:29 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+// const sinon = require('sinon');
+
+// describe("index.js", () => {
+//   let input;
+
+//   beforeEach(function() {
+//     input = document.getElementById('input');
+//     sinon.spy(input, 'addEventListener');
+//   })
+
+//   it("binds an event listener in addingEventListener()", () => {
+//     addingEventListener();
+//     expect(input.addEventListener.called).to.be.true;
+//   })
+// })
